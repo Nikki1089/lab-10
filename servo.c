7e9aac7846d6676e16b87148acd4100abf32eac6
Nikki1089 <Nicole04@iastate.edu>
2026-04-15 16:02:35 -0500

    Update servo.c

--- servo.c
+++ servo.c
@@ -2,33 +2,38 @@
 #include "Timer.h"
 #include "lcd.h"
 
-int RIGHT = 7500; 
-int LEFT = 35000;
+int pulse_period  = 320000;
+int mid_width = 22000;
 
 void servo_init(){
-    SYSCTL_RCGCTIMER_R |= 0x2;
-    SYSCTL_RCGCGPIO_R |= 0x2;
-    while((SYSCTL_PRGPIO_R & 0x2) != 0x2){}
-    GPIO_PORTB_DEN_R |= 0x20;
-    GPIO_PORTB_DIR_R |= 0x20;
-    GPIO_PORTB_AFSEL_R |= 0x20;
-    GPIO_PORTB_PCTL_R |= 0x700000;
-    TIMER1_CTL_R &= ~0x100;
-    TIMER1_CFG_R |= 0x4;
-    TIMER1_TBMR_R &= ~0x4;
-    TIMER1_TBMR_R |= 0b1010;
-    TIMER1_TBILR_R = 0xE200;
-    TIMER1_TBPMR_R = 0x04;
-    TIMER1_TBMATCHR_R = 0xC4B4;
-    TIMER1_CTL_R |= 0x100;
+    SYSCTL_RCGCTIMER_R |= 0x02;   // Timer1
+   SYSCTL_RCGCGPIO_R  |= 0x02;   // Port B
+     TIMER1_CTL_R &= ~TIMER_CTL_TBEN;
+     GPIO_PORTB_AFSEL_R |= 0x20;       
+     GPIO_PORTB_PCTL_R &= ~0x00F00000; 
+     GPIO_PORTB_PCTL_R |=  0x00700000;  
+    GPIO_PORTB_DIR_R   |= 0x20;    
+    GPIO_PORTB_DEN_R   |= 0x2
+
+     TIMER1_TBMR_R &= ~TIMER_TBMR_TBCMR;
+     TIMER1_CFG_R |= 0x4;
+     TIMER1_TBMR_R |= 0x2;
+     TIMER1_TBMR_R |= TIMER_TBMR_TBAMS; 
+     TIMER1_TBILR_R = pulse_period & 0xFFFF; 
+     TIMER1_TBPR_R = pulse_period >> 16 ;
+
+     TIMER1_TBMATCHR_R = (pulse_period - mid_width ) & 0xFFFF;
+     TIMER1_TBPMR_R = (pulse_period - mid_width) >> 16;
+     TIMER1_CTL_R |= TIMER_CTL_TBEN; 
 }
 
 void servo_move(uint16_t degrees){
-    int cycles = (((LEFT - RIGHT) / 180) * degrees) + RIGHT;
-    int match = 320000 - cycles;
-
-
-    TIMER1_TBPMR_R = (match >> 16) & 0xFF;
-    TIMER1_TBMATCHR_R = (match & 0xFFFF);
+   {
+     unsigned pulse_width;
+     pulse_width = (36000 -7500)*degrees /180.0 + 7500;    // 7500->0 degree    36000->180 degree
+     TIMER1_TBMATCHR_R = (pulse_period - pulse_width ) & 0xFFFF;
+     TIMER1_TBPMR_R = (pulse_period - pulse_width) >> 16;
+     timer_waitMillis(500);
+}
 }
 
